0091aad2c594ef9640c18013c59efa7cd004e52c
Nebyu Samuel <kiflenegasi@yahoo.com>
2024-02-06 13:10:48 +0300

    Handle errors in async callbacks

---
+++ index.js
@@ -0,0 +1,41 @@
+// FS
+import { readFile } from "fs";
+
+// Path
+import path from "path";
+
+/**
+ * Read a json file and parse it
+ * @param {string} fileName
+ * @param {Function} callback
+ */
+
+((fileName, callback) => {
+  // File path
+  const filePath = path.join(process.cwd(), `data/${fileName}`);
+
+  // Read
+  readFile(filePath, "utf-8", (err, data) => {
+    // Parse data
+    let parsedData;
+
+    if (err) {
+      callback(err);
+    }
+
+    callback(undefined, JSON.parse(data));
+  });
+})("data.json", (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Error caught");
+  console.log(`Message - ${err.message}`);
+  console.log(`Error - ${err.stack}`);
+  process.exit(1);
+});
